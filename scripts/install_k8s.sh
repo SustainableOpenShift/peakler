ac478b0a24a95ce515d4e7586bca8d0003c75e34
Han Dong <hand32@node0.hand32-190143.bayopsys.emulab.net>
2024-02-07 14:12:55 -0700

    install_k8s.sh

---
+++ scripts/install_k8s.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+set -x
+
+sudo apt-get update
+
+# taken from https://www.cherryservers.com/blog/install-kubernetes-on-ubuntu
+
+# disable swap
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+lsblk
+
+# Set up the IPV4 bridge on all nodes
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+# Install kubelet, kubeadm, and kubectl on each node
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo mkdir /etc/apt/keyrings
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt install -y kubelet=1.28.2-00 kubeadm=1.28.2-00 kubectl=1.28.2-00
+
+# prevents them from being updated, upgraded, etc
+sudo apt-mark hold kubeadm kubelet kubectl
+
+# install docker
+sudo apt install docker.io
+sudo mkdir /etc/containerd
+sudo sh -c "containerd config default > /etc/containerd/config.toml"
+sudo sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
+sudo systemctl restart containerd.service
+sudo systemctl enable kubelet.service
+sudo systemctl restart kubelet.service
+sudo systemctl enable kubelet.service
+
+# initialize kubernetes services
+sudo kubeadm config images pull
