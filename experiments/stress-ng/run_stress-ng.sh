eda70d140913c4aed5abf365c8292face73cf9d2
Han <handong@bu.edu>
2024-02-18 23:10:29 +0000

    update stress-ng

--- experiments/stress-ng/run_stress-ng.sh
+++ experiments/stress-ng/run_stress-ng.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
-#set -x
+set -x
+TIME=60
+mkdir results
 
-sudo rm /tmp/rapl.log
-
-sudo systemctl restart rapl_log
-
-#for (( i=1; i<=$(nproc); i+=1 )); do
-for (( i=1; i<=2; i+=1 )); do
-    stress-ng --cpu $i --cpu-method int64 -t 120s
+for ((iter=0; iter<3; iter+=1 )); do
+    for (( i=1; i<=$(nproc); i+=1 )); do
+	sudo rm /tmp/rapl.log
+	sudo systemctl restart rapl_log
+	
+	stress-ng --cpu $i --cpu-method int64 -t ${TIME}s
+	
+	sudo systemctl stop rapl_log
+	cp /tmp/rapl.log "results/stress-ng.iter${iter}.numcpus${i}.TurboOn.HyperThreadOn.int64.60"
+    done
 done
 
-sudo systemctl stop rapl_log
+sleep 60
+# disable HyperThreads
+echo off | sudo tee /sys/devices/system/cpu/smt/control
+
+# disable TurboBoost
+echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+sleep 60
+
+for ((iter=0; iter<3; iter+=1 )); do
+    for (( i=1; i<=$(nproc); i+=1 )); do
+	sudo rm /tmp/rapl.log
+	sudo systemctl restart rapl_log
+	
+	stress-ng --cpu $i --cpu-method int64 -t ${TIME}s
+	
+	sudo systemctl stop rapl_log
+	cp /tmp/rapl.log "results/stress-ng.iter${iter}.numcpus${i}.TurboOff.HyperThreadOff.int64.60"
+    done
+done
+
