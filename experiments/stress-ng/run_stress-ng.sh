43848a4ec8a5e3ae96ad06de939d46bd66d2fe77
Han <handong@bu.edu>
2024-02-26 18:48:30 +0000

    add stress-ng vm results

--- experiments/stress-ng/run_stress-ng.sh
+++ experiments/stress-ng/run_stress-ng.sh
@@ -1,38 +1,61 @@
 #!/bin/bash
 
-set -x
-TIME=60
+#set -x
+
+export NCORES=${NCORES:=$(nproc)}
+export TIME=${TIME:=60}
+export METHOD=${METHOD:="int64"}
+export NITERS=${NITERS:=3}
+
+
 mkdir results
 
-for ((iter=0; iter<3; iter+=1 )); do
-    for (( i=1; i<=$(nproc); i+=1 )); do
+# enable HyperThreads
+echo on | sudo tee /sys/devices/system/cpu/smt/control
+# enable TurboBoost
+echo "0" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
+sleep 60
+
+NCORES=$(nproc)
+
+echo "NITERS: ${NITERS}"
+echo "NCORES: ${NCORES}"
+echo "TIME: ${TIME}"
+
+for ((iter=0; iter<${NITERS}; iter+=1 )); do
+    for (( i=1; i<=${NCORES}; i+=1 )); do
+	#echo "stress-ng --vm ${i} --vm-bytes 2G --mmap ${i} --mmap-bytes 2G --page-in -t ${TIME}s"
+	#echo "results/stress-ng.iter${iter}.numcpus${i}.TurboOn.HyperThreadOn.vm2G.${TIME}"
 	sudo rm /tmp/rapl.log
 	sudo systemctl restart rapl_log
 	
-	stress-ng --cpu $i --cpu-method int64 -t ${TIME}s
+	#stress-ng --cpu $i --cpu-method int64 -t ${TIME}s
+	stress-ng --vm ${i} --vm-bytes 2G --mmap ${i} --mmap-bytes 2G --page-in -t ${TIME}s
 	
 	sudo systemctl stop rapl_log
-	cp /tmp/rapl.log "results/stress-ng.iter${iter}.numcpus${i}.TurboOn.HyperThreadOn.int64.60"
+	
+	#cp /tmp/rapl.log "results/stress-ng.iter${iter}.numcpus${i}.TurboOn.HyperThreadOn.int64.60"
+	cp /tmp/rapl.log "results/stress-ng.iter${iter}.numcpus${i}.TurboOn.HyperThreadOn.vm2G.${TIME}"
     done
 done
 
-sleep 60
 # disable HyperThreads
 echo off | sudo tee /sys/devices/system/cpu/smt/control
-
 # disable TurboBoost
 echo "1" | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 sleep 60
 
-for ((iter=0; iter<3; iter+=1 )); do
-    for (( i=1; i<=$(nproc); i+=1 )); do
+for ((iter=0; iter<${NITERS}; iter+=1 )); do
+    for (( i=1; i<=${NCORES}; i+=1 )); do
 	sudo rm /tmp/rapl.log
 	sudo systemctl restart rapl_log
 	
-	stress-ng --cpu $i --cpu-method int64 -t ${TIME}s
+	#stress-ng --cpu $i --cpu-method int64 -t ${TIME}s
+	stress-ng --vm ${i} --vm-bytes 2G --mmap ${i} --mmap-bytes 2G --page-in -t ${TIME}s
 	
 	sudo systemctl stop rapl_log
-	cp /tmp/rapl.log "results/stress-ng.iter${iter}.numcpus${i}.TurboOff.HyperThreadOff.int64.60"
+	cp /tmp/rapl.log "results/stress-ng.iter${iter}.numcpus${i}.TurboOff.HyperThreadOff.vm2G.${TIME}"
+	#cp /tmp/rapl.log "results/stress-ng.iter${iter}.numcpus${i}.TurboOff.HyperThreadOff.int64.60"
     done
 done
 
