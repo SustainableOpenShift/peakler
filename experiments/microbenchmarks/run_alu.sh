f815138a134b3d3741683fb57abd1005c0cb613f
Han <handong@bu.edu>
2024-03-10 21:21:21 +0000

    update alu

--- experiments/microbenchmarks/run_alu.sh
+++ experiments/microbenchmarks/run_alu.sh
@@ -78,15 +78,15 @@ mainParallel () {
     for (( p=1; p<=$(($(nproc)-86)); p++ )); do
 	for (( i=0; i<$NROUNDS; i++ )); do
 	    echo "Round ${i}"
-	    curl "${ENDPOINT}/metrics" > "results/runALU.ITER${NITERS}.PARALLEL${p}.ROUND${i}.START"
-	    sleep 1
-	    for (( j=1; j<=$p; j++ )); do
-		taskset -c $(($(nproc)-$j)) ./build/runALU ${NITERS} &
-	    done
-	    wait
-	    sleep 1
-	    curl "${ENDPOINT}/metrics" > "results/runALU.ITER${NITERS}.PARALLEL${p}.ROUND${i}.END"
-	    sleep 5
+	    #curl "${ENDPOINT}/metrics" > "results/runALU.ITER${NITERS}.PARALLEL${p}.ROUND${i}.START"
+	    #sleep 1
+	    #for (( j=1; j<=$p; j++ )); do
+#		taskset -c $(($(nproc)-$j)) ./build/runALU ${NITERS} &
+#	    done
+#	    wait
+#	    sleep 1
+#	    curl "${ENDPOINT}/metrics" > "results/runALU.ITER${NITERS}.PARALLEL${p}.ROUND${i}.END"
+#	    sleep 5
 	done
     done
 }
