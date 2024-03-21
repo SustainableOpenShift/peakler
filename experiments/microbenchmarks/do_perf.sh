6327186be5ea4f973017d4a1089205c1b7f274f1
hand32 <hand32@node1.hand32-197136.bayopsys-pg0.apt.emulab.net>
2024-03-21 16:07:05 -0600

    update do_perf

--- experiments/microbenchmarks/do_perf.sh
+++ experiments/microbenchmarks/do_perf.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-NPARALLEL=$(nproc)
+set -x
 
-for (( p=1; p<=$NPARALLEL; p++ )); do
-    echo "🟢🟢 perf stat -a -e instructions,cache-misses,ref-cycles,power/energy-pkg/,power/energy-ram/ -x, -r 3 -o runALUPerfStat.ITER9000.PARALLEL${p}.REPEAT3 ./run_alu.sh -i 9000 -p $p  🟢🟢"
-    sleep 5
-    perf stat -a -e instructions,cache-misses,ref-cycles,power/energy-pkg/,power/energy-ram/ -x, -r 3 -o "runALUPerfStat.ITER9000.PARALLEL${p}.REPEAT3" ./run_alu.sh -i 9000 -p $p
-    sleep 10
-done
+function orig()
+{
+    NPARALLEL=$(nproc)
+
+    for (( p=1; p<=$NPARALLEL; p++ )); do
+	echo "🟢🟢 perf stat -a -e instructions,cache-misses,ref-cycles,power/energy-pkg/,power/energy-ram/ -x, -r 3 -o runALUPerfStat.ITER9000.PARALLEL${p}.REPEAT3 ./run_alu.sh -i 9000 -p $p  🟢🟢"
+	sleep 5
+	perf stat -a -e instructions,cache-misses,ref-cycles,power/energy-pkg/,power/energy-ram/ -x, -r 3 -o "runALUPerfStat.ITER9000.PARALLEL${p}.REPEAT3" ./run_alu.sh -i 9000 -p $p
+	sleep 10
+    done
+}
+
+function parallel2()
+{
+    #perf stat -a -e instructions,cache-misses,ref-cycles,power/energy-pkg/,power/energy-ram/ -x, -r 3 ./build/runALU 9000 &
+    for (( p=1; p<=3; p++ )); do
+	#perf stat -e instructions,cache-misses,cycles taskset -c 1 ./build/runALU 9000 &
+	#perf stat -e instructions,cache-misses,cycles taskset -c 7 ./build/runALU 18000 &
+	perf stat -a -e power/energy-pkg/,power/energy-ram/ taskset -c 1 ./build/runALU 9000 &
+	perf stat -a -e power/energy-pkg/,power/energy-ram/ taskset -c 7 ./build/runALU 18000 &	
+	wait
+    done
+}
+
+"$@"
