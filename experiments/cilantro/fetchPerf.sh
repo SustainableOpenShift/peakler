11d1b033b45b7a447e6cd1786f4eeba960acd466
hand32 <hand32@client1.hand32-211847.bayopsys-pg0.wisc.cloudlab.us>
2024-07-16 09:42:18 -0500

    update script

--- experiments/cilantro/fetchPerf.sh
+++ experiments/cilantro/fetchPerf.sh
@@ -3,17 +3,19 @@
 #set -x
 
 export NITERS=${NITERS:=1}
-export WORKERS=${WORKERS:="192.168.1.2 192.168.1.3 192.168.1.4"}
+export WORKERS=${WORKERS:="192.168.1.2 192.168.1.3"}
 export SCHED=${SCHED:="default"}
 
 mkdir -p $SCHED/results
 
 for w in $WORKERS; do
-    ssh $w "nohup perf stat -a -e power/energy-pkg/ -x, -I 1000 sleep 14400 > perf.log 2>&1 &"
+    ssh $w "nohup perf stat -a -e power/energy-pkg/ -x, -I 1000 sleep $((NITERS*3600)) > perf.log 2>&1 &"
 done
 
-
-for ((t=0;t<$NITERS;t++)); do    
+for ((t=0;t<$NITERS;t++)); do
+    echo "Sleeping......"
+    sleep 3600
+    
     kubectl logs $(kubectl get pods | awk '/cilantroscheduler/ {print $1;exit}') > $SCHED/results/cilantroscheduler.log
     kubectl -c 'hr-client' logs $(kubectl get pods | awk '/hr-client/ {print $1;exit}') > $SCHED/results/hr-client.log
     kubectl -c 'cilantro-hr-client' logs $(kubectl get pods | awk '/hr-client/ {print $1;exit}') > $SCHED/results/cilantro-hr-client.log
@@ -23,9 +25,7 @@ for ((t=0;t<$NITERS;t++)); do
 
     kubectl get pods -o wide > $SCHED/results/kubectl.pods.$t
     kubectl get pods -o wide --all-namespaces > $SCHED/results/kubectl-all-namespaces.pods.$t
-    
-    echo "Sleeping for 1 hour ......"
-    sleep 3600
+
     for w in $WORKERS; do
     	scp -r $w:~/perf.log $SCHED/results/perf.$w.log
     done
