54e62a4110bc6b8d4ec0fda510c9df3b949ebb2f
hand32 <hand32@node0.hand32-220192.bayopsys-pg0.apt.emulab.net>
2024-09-27 14:22:26 -0600

    stashing yamls for cluster-share experiments

---
+++ experiments/cluster-share/cray_yamls/extract.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for n in $(kubectl get -o=name pvc,configmap,serviceaccount,secret,ingress,service,deployment,statefulset,hpa,job,cronjob)
+do
+    mkdir -p $(dirname $n)
+    kubectl get $n -o yaml | ./kubectl-neat > $n.yaml
+done
